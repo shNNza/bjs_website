8b7fe60c8e271edc9206ea13808e493b8e37b635
Kyle Whitfield <kyle@bluechiptech.co.za>
2025-09-08 09:14:58 +0200

    second commit

--- bjs_website/wsgi.py
+++ bjs_website/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bjs_website.settings')
 
 application = get_wsgi_application()
+
+# just a comment
